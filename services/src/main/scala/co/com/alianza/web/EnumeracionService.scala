a4bd044adc57229f0a8aaa2eacbbae980dae542c
Hernando Correa <hernandocorrea@seven4n.com>
2014-11-11 18:51:41 -0500

    Agregar tipos de documento T y F

--- services/src/main/scala/co/com/alianza/web/EnumeracionService.scala
+++ services/src/main/scala/co/com/alianza/web/EnumeracionService.scala
@@ -20,7 +20,7 @@ class EnumeracionService extends Directives with AlianzaCommons  {
     path(enumeracion/tiposIdentificacion) {
         get {
             complete {
-              val list = List(new TipoIdentificaciones( 1 ,"CC" ),new TipoIdentificaciones( 2 ,"CE" ), new TipoIdentificaciones( 3 ,"NIT" ))
+              val list = List(new TipoIdentificaciones( 1 ,"CC" ),new TipoIdentificaciones( 2 ,"CE" ), new TipoIdentificaciones( 3 ,"NIT" ), new TipoIdentificaciones( 4 ,"F" ), new TipoIdentificaciones( 5 ,"T" ))
               JsonUtil.toJson(list)
             }
         }
