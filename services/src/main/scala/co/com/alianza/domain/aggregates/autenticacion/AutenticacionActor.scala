7e3be3a189c576cd67076bdcc7d64d1d73012740
macsign <manuelchavez@seven4n.com>
2014-12-17 16:04:38 -0500

    Cambios menores

--- services/src/main/scala/co/com/alianza/domain/aggregates/autenticacion/AutenticacionActor.scala
+++ services/src/main/scala/co/com/alianza/domain/aggregates/autenticacion/AutenticacionActor.scala
@@ -38,6 +38,8 @@ class AutenticacionActorSupervisor extends Actor with ActorLogging {
 
   def receive = {
 
+    case m: AutenticarUsuarioEmpresaMessage => autenticacionUsuarioEmpresaActor forward m
+
     case message: Any =>
       autenticacionActor forward message
 
