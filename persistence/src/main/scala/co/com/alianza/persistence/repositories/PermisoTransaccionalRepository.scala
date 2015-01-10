e77d2f838522608507baca17df54e7d9c98793fc
macsign <manuelchavez@seven4n.com>
2015-01-09 19:16:18 -0500

    Agregado método para guardar los autorizadores de un permiso

--- persistence/src/main/scala/co/com/alianza/persistence/repositories/PermisoTransaccionalRepository.scala
+++ persistence/src/main/scala/co/com/alianza/persistence/repositories/PermisoTransaccionalRepository.scala
@@ -13,13 +13,14 @@ import scala.util.Try
 class PermisoTransaccionalRepository ( implicit executionContext: ExecutionContext) extends AlianzaRepository {
 
   val tabla = TableQuery[PermisoTransaccionalUsuarioEmpresarialTable]
+  val tablaAutorizadores = TableQuery[PermisoTransaccionalUsuarioEmpresarialAutorizadorTable]
 
   /**
    * Crea o actualiza un permiso
    * @param permiso
    * @return
    */
-  def guardarPermiso(permiso: PermisoTransaccionalUsuarioEmpresarial) = loan {
+  def guardarPermiso(permiso: PermisoTransaccionalUsuarioEmpresarial, idsAgentes: Option[List[Int]] = None) = loan {
     implicit session =>
       resolveTry(
         Try {
@@ -35,6 +36,24 @@ class PermisoTransaccionalRepository ( implicit executionContext: ExecutionConte
         },
         "Guardar permiso transaccional de agente"
       )
+  }
+
+  private[this] def guardarAgentesPermiso(permiso: PermisoTransaccionalUsuarioEmpresarial, idsAgentes: Option[List[Int]] = None) = {
+    if(idsAgentes.isDefined && !idsAgentes.get.isEmpty){
+      val ids = idsAgentes.get
+      val q = for {
+        au <- tablaAutorizadores if au.idEncargo === permiso.idEncargo && au.idAgente === permiso.idAgente && au.tipoTransaccion === permiso.tipoTransaccion
+      } yield au
+      val ausentes = ids.diff(q.list.map(_.idAutorizador))
+      val q2 = for{
+        aut <- tablaAutorizadores if aut.idAutorizador in ausentes
+      } yield _
+      if(regMod==0){
+        tabla += permiso
+        1
+      } else
+        regMod
+    }
   }
 
 }
