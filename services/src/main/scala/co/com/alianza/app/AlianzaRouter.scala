b53df3a1843ec89a0801ed835aa4ab73d02868ea
ricardom57 <ricardomestre@seven4n.com>
2015-02-27 16:45:40 -0500

    Update AlianzaRouter.scala

--- services/src/main/scala/co/com/alianza/app/AlianzaRouter.scala
+++ services/src/main/scala/co/com/alianza/app/AlianzaRouter.scala
@@ -17,7 +17,7 @@ class AlianzaRouter extends HttpServiceActor with RouteConcatenation with CrossH
 
   val routes =
     new AutorizacionService().route ~
-    new AutenticacionService( ).route ~
+    new AutenticacionService().route ~
     new ConfrontaService().route ~
     new EnumeracionService().route ~
     new UsuarioService().route ~
@@ -27,8 +27,8 @@ class AlianzaRouter extends HttpServiceActor with RouteConcatenation with CrossH
     authenticate(authenticateUser) {
       user =>
         new IpsUsuariosService().route(user) ~
-        new AdministrarContrasenaService().secureRoute(user) ~
-        new AutenticacionService().routeAutenticado( user )
+        new AdministrarContrasenaService().secureRoute(user) /*~
+        new AutenticacionService().routeAutenticado( user )*/
     }
 
   def receive = runRoute(
