3da438c3ded99954eb10e371506de65261874af7
David Alberto Montaño Fetecua <davidmontano@seven4n.com>
2015-06-18 21:23:06 -0500

    ajustes en loging de autenticación de fiduciaria y se termina el de valores

--- services/src/main/scala/co/com/alianza/web/AutenticacionService.scala
+++ services/src/main/scala/co/com/alianza/web/AutenticacionService.scala
@@ -1,6 +1,8 @@
 package co.com.alianza.web
 
 import co.com.alianza.infrastructure.auditing.AuditingHelper._
+import co.com.alianza.util.clave.Crypto
+import enumerations.AppendPasswordUser
 import spray.routing.{RequestContext, Directives}
 import co.com.alianza.app.{ CrossHeaders, AlianzaCommons }
 import co.com.alianza.infrastructure.messages.{IpsUsuarioMessagesJsonSupport, AutenticacionMessagesJsonSupport, AutenticarMessage, AgregarIPHabitualUsuario}
@@ -17,7 +19,7 @@ class AutenticacionService extends Directives with AlianzaCommons with CrossHead
           autenticacion =>
             respondWithMediaType(mediaType) {
               clientIP { ip =>
-                mapRequestContext((r: RequestContext) => requestWithAuiditing(r, "Fiduciaria", "autenticacion-fiduciaria", ip.value, kafkaActor)) {
+                mapRequestContext((r: RequestContext) => requestWithAuiditing(r, "Fiduciaria", "autenticacion-fiduciaria", ip.value, kafkaActor, autenticacion.copy( password = Crypto.hashSha512(autenticacion.password.concat(AppendPasswordUser.appendUsuariosFiducia)), clientIp = Some(ip.value)))) {
                   val nuevaAutenticacion = autenticacion.copy(clientIp = Some(ip.value))
                   requestExecute(nuevaAutenticacion, autenticacionActor)
                 }
