2041d34089e8805f30d42ecc6f8ab7093b39e280
jonathanforero <jonathanforero@seven4n.com>
2015-06-23 16:20:10 -0500

    Permisos sobre Programacion de pagos para Agente Emporesarial

--- common/src/main/scala/co/com/alianza/commons/enumerations/TiposTransacciones.scala
+++ common/src/main/scala/co/com/alianza/commons/enumerations/TiposTransacciones.scala
@@ -11,5 +11,6 @@ object TiposTransacciones extends Enumeration {
   val `INSCRIPCIÓN` = TipoTransaccion( 2 , "Inscripción" )
   val `PAGOS MASIVOS` = TipoTransaccion( 3 , "Pagos Masivos" )
   val `TRANSFERENCIAS Y RETIROS` = TipoTransaccion( 4 ,"Transferencias y Retiros" )
+  val `PROGRAMACION DE PAGOS` = TipoTransaccion( 6 ,"Programación de pagos" )
 
 }
