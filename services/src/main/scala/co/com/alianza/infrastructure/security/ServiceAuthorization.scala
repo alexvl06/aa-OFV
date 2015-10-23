427124cd0ee568b7156dcd3ece72d601f9eb5dcd
jonathanforero <jonathanforero@seven4n.com>
2015-10-23 15:09:27 -0500

    Se retiran tags producto de cherrypick (ddd36531a4396d404c4b3599180978bacfb5c94a)

--- services/src/main/scala/co/com/alianza/infrastructure/security/ServiceAuthorization.scala
+++ services/src/main/scala/co/com/alianza/infrastructure/security/ServiceAuthorization.scala
@@ -44,10 +44,7 @@ trait ServiceAuthorization {
       } else {
         var util = new AesUtil(CryptoAesParameters.KEY_SIZE, CryptoAesParameters.ITERATION_COUNT)
         var decryptedToken = util.decrypt(CryptoAesParameters.SALT, CryptoAesParameters.IV, CryptoAesParameters.PASSPHRASE, token.get.value)
-<<<<<<< HEAD
 
-=======
->>>>>>> ddd3653... Arreglos reportados por QA HU token encriptado
         val tipoCliente = Token.getToken(decryptedToken).getJWTClaimsSet.getCustomClaim("tipoCliente").toString
         val p = promise[Any]
         var futuro: Future[Any] = null
