1d6766e953af6de0b4119f40924f26e7494b6a05
jonathanforero <jonathanforero@seven4n.com>
2016-05-04 11:20:40 -0500

    Arreglo case class para repsuesta de SP de Grupos

--- services/src/main/scala/co/com/alianza/infrastructure/anticorruption/grupos/DataAccessTranslator.scala
+++ services/src/main/scala/co/com/alianza/infrastructure/anticorruption/grupos/DataAccessTranslator.scala
@@ -1,6 +1,6 @@
 package co.com.alianza.infrastructure.anticorruption.grupos
 
-import co.com.alianza.infrastructure.dto.Cliente
+import co.com.alianza.infrastructure.dto.{ MiembroGrupo, Cliente }
 import co.com.alianza.util.json.MarshallableImplicits._
 
 /**
@@ -9,8 +9,8 @@ import co.com.alianza.util.json.MarshallableImplicits._
  */
 object DataAccessTranslator {
   def translateCliente(clienteJson: String): Option[Cliente] = {
-    val result = clienteJson.fromJson[Array[Cliente]]
-    if (result nonEmpty) Some(result(0)) else None
+    val result = clienteJson.fromJson[Array[MiembroGrupo]]
+    if (result nonEmpty) Some(result(0).toCliente) else None
   }
 }
 
--- services/src/main/scala/co/com/alianza/infrastructure/dto/Cliente.scala
+++ services/src/main/scala/co/com/alianza/infrastructure/dto/Cliente.scala
@@ -1,9 +1,12 @@
 package co.com.alianza.infrastructure.dto
 
-import org.joda.time.DateTime
-
 /**
  *
  * @author smontanez
  */
 case class Cliente(wcli_nombre: String, wcli_person: String, wcli_estado: String, wcli_estado_descri: String, wcli_dir_correo: String, wcli_ident_replegal: String, wcli_cias_pagos_masivos: String)
+
+case class MiembroGrupo(nombre: String, correo: String, estado: String, tipo: String) {
+  def toCliente = Cliente(nombre, tipo, estado, "", correo, "", "")
+}
+
