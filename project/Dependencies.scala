67c5b6d4270965c5d0c2d946bbf3316feef97ce2
Alexa <alexandraavendano@seven4n.com>
2016-05-05 13:47:09 -0500

    Exclusiones de librerias que generaban conflicto

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,7 @@
 import sbt._
 
 object Dependencies {
-
-	/**
-	 * Scala basic libraries
-	 **/
-
+  
 	private[Dependencies] implicit class Exclude(module: ModuleID) {
 
 		def kafkaExclusions: ModuleID = {
@@ -17,18 +13,36 @@ object Dependencies {
 			)
 		}
 
-		def scalaParserCombinatorsExclude: ModuleID = module.logScalaExclude.exclude("org.scala-lang.modules", "scala-parser-combinators_2.11")
+		def jacksonExclude : ModuleID = {
+			module.logScalaExclude.excludeAll(
+				ExclusionRule("com.fasterxml.jackson.core", "jackson-databind"),
+        ExclusionRule("com.fasterxml.jackson.core", "jackson-core")
+			)
+		}
 
-		//def scalaReflectExclude: ModuleID = module..exclude("org.scala-lang", "scala-reflect")
+    def nettyExclude : ModuleID = module.logScalaExclude.exclude("io.netty","netty")
 
-		def logScalaExclude : ModuleID = module.logbackExclude.scalaLibraryExclude
+    def scalaParserCombinatorsExclusions: ModuleID = module.logScalaExclude.exclude("org.scala-lang.modules", "scala-parser-combinators_2.11")
 
-		def logbackExclude: ModuleID = {
+    def scalaxmlExclude : ModuleID = module.logScalaExclude.exclude("org.scala.lang.modules","scala-xml_2.11")
+
+    def slickExclude : ModuleID = module.jodaTimeExclusions.excludeAll(ExclusionRule("com.github.tminglei","slick-pg_core_2.11"))
+
+    def logScalaExclude : ModuleID = module.logbackExclude.scalaLibraryExclude
+
+    def logbackExclude: ModuleID = {
 			module.log4jExclude.excludeAll(
 				ExclusionRule("ch.qos.logback", "logback-classic"),
 				ExclusionRule("ch.qos.logback", "logback-core")
 			)
 		}
+
+    def jodaTimeExclusions : ModuleID = {
+      module.logScalaExclude.excludeAll(
+        ExclusionRule("joda-time","joda-time"),
+        ExclusionRule("org-joda","joda-convert")
+      )
+    }
 
 		def log4jExclude: ModuleID = {
 			module.excludeAll(
@@ -53,25 +67,25 @@ object Dependencies {
 	private[Dependencies] object Compile {
 		import Versions._
 
-		val akkaActor 			= "com.typesafe.akka" 	    			 %% "akka-actor" 			 		 	% akkaVersion logScalaExclude
-		val akkaSlf4j 			= "com.typesafe.akka" 	    		 	 %% "akka-slf4j"       			% akkaVersion
-		val akkaCluster 		= "com.typesafe.akka"     			 	 % "akka-cluster_2.11"      		% akkaClusterVersion
-		val logbackClassic 	= "ch.qos.logback"     							% "logback-classic"  				% akkaLogbackVersion
-		val scalaLibrary 		= "org.scala-lang" 					      	% "scala-library" 					% commonScalaVersion
-		val scalaCompiler 	= "org.scala-lang" 					      	% "scala-compiler" 					% commonScalaVersion
-		val scalaIOCore 		= "com.github.scala-incubator.io" 	%% "scala-io-core" 					% scalaioVersion logScalaExclude
-		val scalaIOFile 		= "com.github.scala-incubator.io" 	%% "scala-io-file" 					% scalaioVersion logScalaExclude
-		val scalazLib 			= "org.scalaz"				      			%%  "scalaz-core" 					% scalazVersion
-		val sprayCan 				= "io.spray"           		  			 %% "spray-can"    		  		% sprayVersion
-		val sprayRouting 		= "io.spray"           		  			 %% "spray-routing-shapeless2"  % sprayVersion
-		val sprayClient 		= "io.spray"           		  			 %% "spray-client"   				% sprayVersion
-		val sprayHttp 			= "io.spray"           		  			 %% "spray-http"    	  		% sprayVersion
-		val sprayHttpx 			= "io.spray"           		  			 %% "spray-httpx"   	  		% sprayVersion
+		val akkaActor 			= "com.typesafe.akka" 	    			 %% "akka-actor" 			 		 	% akkaVersion nettyExclude
+		val akkaSlf4j 			= "com.typesafe.akka" 	    		 	 %% "akka-slf4j"       			% akkaVersion logScalaExclude
+		val akkaCluster 		= "com.typesafe.akka"     			 	 % "akka-cluster_2.11"      % akkaClusterVersion nettyExclude
+		val logbackClassic 	= "ch.qos.logback"     							% "logback-classic"  			% akkaLogbackVersion
+		val scalaLibrary 		= "org.scala-lang" 					      	% "scala-library" 				% commonScalaVersion
+		val scalaCompiler 	= "org.scala-lang" 					      	% "scala-compiler" 				% commonScalaVersion
+		val scalaIOCore 		= "com.github.scala-incubator.io" 	%% "scala-io-core" 				% scalaioVersion logScalaExclude
+		val scalaIOFile 		= "com.github.scala-incubator.io" 	%% "scala-io-file" 				% scalaioVersion logScalaExclude
+		val scalazLib 			= "org.scalaz"				      			%%  "scalaz-core" 					% scalazVersion logScalaExclude
+		val sprayCan 				= "io.spray"           		  			 %% "spray-can"    		  		% sprayVersion logScalaExclude
+		val sprayRouting 		= "io.spray"           		  			 %% "spray-routing-shapeless2"  % sprayVersion logScalaExclude
+		val sprayClient 		= "io.spray"           		  			 %% "spray-client"   				% sprayVersion logScalaExclude
+		val sprayHttp 			= "io.spray"           		  			 %% "spray-http"    	  		% sprayVersion logScalaExclude
+		val sprayHttpx 			= "io.spray"           		  			 %% "spray-httpx"   	  		% sprayVersion scalaxmlExclude
 		val sprayJsonLib 		= "io.spray" 					      			 %% "spray-json" 		    		% sprayJsonVersion logScalaExclude
-		val sprayCaching 		= "io.spray"           						 % "spray-caching"     		  % sprayVersion
+		val sprayCaching 		= "io.spray"           						 % "spray-caching"     		  % sprayVersion logScalaExclude
 
 		val scalateLib		  = "org.fusesource.scalate"      		% "scalate-core_2.10" 		% Versions.scalateVersion
-		val shapelessLib 			= "com.chuusai"				      			%% "shapeless" 		   		 	% shapelessVersion
+		val shapelessLib 			= "com.chuusai"				      			%% "shapeless" 		   		 	% shapelessVersion logScalaExclude
 
 		val commonsLogging  = "commons-logging" 			  			 % "commons-logging" 	  		% apacheLoginVersion
 		val commonsLang3Lib = "org.apache.commons" 		  			 % "commons-lang3" 	    		% apacheLangVersion
@@ -85,10 +99,10 @@ object Dependencies {
 
 		val ojdbc 					= "oracle"					        			 % "ojdbc"				      		% ojdbcVersion
 
-		val playJsonLib 					= "com.typesafe.play" 		       	%% "play-json" 			    	% playJsonVersion
+		val playJsonLib 					= "com.typesafe.play" 		       	%% "play-json" 			    	% playJsonVersion jodaTimeExclusions
 		val jacksonDatabindLib 		= "com.fasterxml.jackson.core"    % "jackson-databind" 		  % jacksonDataBindVersion
-		val jacksonModuleScalaLib = "com.fasterxml.jackson.module" 	%% "jackson-module-scala" 	% jacksonModuleScalaVersion
-		val jsonToken 						= "com.googlecode.jsontoken" 			% "jsontoken" 							% jsonTokenVersion
+		val jacksonModuleScalaLib = "com.fasterxml.jackson.module" 	%% "jackson-module-scala" 	% jacksonModuleScalaVersion jacksonExclude
+		val jsonToken 						= "com.googlecode.jsontoken" 			% "jsontoken" 							% jsonTokenVersion jodaTimeExclusions
 		val ninbus 								= "com.nimbusds"									% "nimbus-jose-jwt" 				% ninbusVersion
 		val jasyptLib 						= "org.jasypt" 										% "jasypt" 									% jasyptVersion
 		val kafkaLib 							= "org.apache.kafka" 							% "kafka_2.10" 							% kafkaVersion kafkaExclusions
@@ -96,21 +110,21 @@ object Dependencies {
 		val slickLib 							= "com.typesafe.slick" 						 %% "slick"                   % slickVersion log4jExclude
 		val postgresqlLib 				= "postgresql"          					 % "postgresql"              % postgreSqlVersion log4jExclude
 		val c3p0Lib 							= "c3p0"                					 % "c3p0"                    % c3p0Version log4jExclude
-		val slickPGLib 						= "com.github.tminglei" 					 %% "slick-pg"           % slickpgVersion log4jExclude
-		val slickPG_jodaTimeLib 	= "com.github.tminglei" 					 %% "slick-pg_joda-time" % slickpgJodaTimeVersion log4jExclude
+		val slickPGLib 						= "com.github.tminglei" 					 %% "slick-pg"           % slickpgVersion slickExclude
+		val slickPG_jodaTimeLib 	= "com.github.tminglei" 					 %% "slick-pg_joda-time" % slickpgJodaTimeVersion slickExclude
 		val recaptcha4j 					= "net.tanesha.recaptcha4j" 			 % "recaptcha4j" 						% "0.0.7"
 	}
 
 	private[Dependencies] object Test {
 		import Versions._
 
-		val akkaTestkit 		= "com.typesafe.akka"       %%  "akka-testkit"        % akkaVersion 				% "test"
+		val akkaTestkit 		= "com.typesafe.akka"       %%  "akka-testkit"        % akkaVersion 				% "test" logScalaExclude
 		val junitLib 				= "junit"                   %  "junit"                % junitVersion    		% "test"
 		val restAssuredLib 	= "com.jayway.restassured"  %  "rest-assured"         % restAssuredVersion  % "test"
-		val scalatestLib 		= "org.scalatest"           %%  "scalatest"      			% scalatestVersion   	% "test"
-		val scalacheckLib 	= "org.scalacheck"          %% "scalacheck"           % scalacheckVersion  	% "test"
-		val sprayTestkitLib = "io.spray" 			          %%  "spray-testkit" 	      % sprayTestkitVersion % "test"
-		val specs2Lib 			= "org.specs2"              %% "specs2"               % specs2Version  			% "test"
+		val scalatestLib 		= "org.scalatest"           %%  "scalatest"      			% scalatestVersion   	% "test" logScalaExclude
+		val scalacheckLib 	= "org.scalacheck"          %% "scalacheck"           % scalacheckVersion  	% "test" logScalaExclude
+		val sprayTestkitLib = "io.spray" 			          %%  "spray-testkit" 	    % sprayTestkitVersion % "test" logScalaExclude
+		val specs2Lib 			= "org.specs2"              %% "specs2"               % specs2Version  			% "test" logScalaExclude
 	}
 
 	import Dependencies.Compile._
