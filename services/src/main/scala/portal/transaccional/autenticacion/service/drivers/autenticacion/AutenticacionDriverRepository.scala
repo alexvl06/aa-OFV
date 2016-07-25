c3f362dae8c7935a75162b45345820b3f7c4dcc5
hernandocorrea <hernandocorrea@s4n.co>
2016-07-25 09:25:18 -0500

    creacion autenticacion driver repository

---
+++ services/src/main/scala/portal/transaccional/autenticacion/service/drivers/autenticacion/AutenticacionDriverRepository.scala
@@ -0,0 +1,14 @@
+package portal.transaccional.autenticacion.service.drivers.autenticacion
+
+import scala.concurrent.{Future, ExecutionContext}
+
+/**
+ * Created by hernando on 25/07/16.
+ */
+case class AutenticacionDriverRepository (implicit val ex : ExecutionContext) extends AutenticacionRepository{
+
+  def autenticar(tipoIdentificacion: Int, numeroIdentificacion: String, password: String, clientIp: String) : Future[String] = {
+    Future{""}
+  }
+
+}
