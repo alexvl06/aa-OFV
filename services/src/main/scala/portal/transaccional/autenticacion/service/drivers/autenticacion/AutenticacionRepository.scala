03d524ef9c36e78df6d385c4b0078a8226f4a291
hernandocorrea <hernandocorrea@s4n.co>
2016-07-25 09:22:04 -0500

    creacion trait autenticacion

---
+++ services/src/main/scala/portal/transaccional/autenticacion/service/drivers/autenticacion/AutenticacionRepository.scala
@@ -0,0 +1,12 @@
+package portal.transaccional.autenticacion.service.drivers.autenticacion
+
+import scala.concurrent.Future
+
+/**
+ * Created by hernando on 25/07/16.
+ */
+trait AutenticacionRepository {
+
+  def autenticar(tipoIdentificacion: Int, numeroIdentificacion: String, password: String, clientIp: String) : Future[String]
+
+}
