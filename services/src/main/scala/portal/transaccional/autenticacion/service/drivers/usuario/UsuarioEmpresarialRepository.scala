12dbe25d6f527a6716ec23c126f65c601501f5f4
hernandocorrea <hernandocorrea@s4n.co>
2016-07-26 20:25:16 -0500

    crear modelo ip empresa

--- services/src/main/scala/portal/transaccional/autenticacion/service/drivers/usuario/UsuarioEmpresarialRepository.scala
+++ services/src/main/scala/portal/transaccional/autenticacion/service/drivers/usuario/UsuarioEmpresarialRepository.scala
@@ -2,6 +2,7 @@ package portal.transaccional.autenticacion.service.drivers.usuario
 
 import java.sql.Timestamp
 
+import co.com.alianza.commons.enumerations.TiposCliente._
 import co.com.alianza.persistence.entities.UsuarioEmpresarial
 
 import scala.concurrent.Future
@@ -13,14 +14,6 @@ trait UsuarioEmpresarialRepository {
 
   def getByIdentityAndUser(identificacion: String, usuario: String): Future[Option[UsuarioEmpresarial]]
 
-  def validarUsuario(usuarioOption: Option[UsuarioEmpresarial], contrasena: String): Future[UsuarioEmpresarial]
-
-  def validarExiste(usuarioOption: Option[UsuarioEmpresarial]): Future[UsuarioEmpresarial]
-
-  def validarEstado(usuario: UsuarioEmpresarial): Future[Boolean]
-
-  def validarContrasena(contrasena: String, usuario: UsuarioEmpresarial): Future[Boolean]
-
   def actualizarToken(idUsuario: Int, token: String): Future[Int]
 
   def actualizarIngresosErroneosUsuario(idUsuario: Int, numeroIntentos: Int): Future[Int]
@@ -28,5 +21,17 @@ trait UsuarioEmpresarialRepository {
   def actualizarIp(idUsuario: Int, ip: String): Future[Int]
 
   def actualizarFechaIngreso(idUsuario: Int, fechaActual: Timestamp): Future[Int]
+
+  def actualizarInfoUsuario(usuario: UsuarioEmpresarial, ip: String): Future[Int]
+
+  def validarUsuario(usuarioOption: Option[UsuarioEmpresarial], contrasena: String, reintentosErroneos: Int): Future[UsuarioEmpresarial]
+
+  def validarExiste(usuarioOption: Option[UsuarioEmpresarial]): Future[UsuarioEmpresarial]
+
+  def validarEstado(usuario: UsuarioEmpresarial): Future[Boolean]
+
+  def validarContrasena(contrasena: String, usuario: UsuarioEmpresarial, reintentosErroneos: Int): Future[Boolean]
+
+  def validarCaducidadContrasena(tipoCliente: TiposCliente, usuario: UsuarioEmpresarial, dias: Int): Future[Boolean]
 
 }
