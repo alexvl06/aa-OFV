3c37d7079fcca97014607f520b2e72663213cf9a
Diego Barato <diegobarato@seven4n.com>
2016-08-05 11:17:03 -0500

    Se ajustan propiedades del login de comercial

--- persistence/src/main/scala/portal/transaccional/fiduciaria/autenticacion/storage/daos/ldap/AlianzaLdapDAO.scala
+++ persistence/src/main/scala/portal/transaccional/fiduciaria/autenticacion/storage/daos/ldap/AlianzaLdapDAO.scala
@@ -1,14 +1,16 @@
 package portal.transaccional.fiduciaria.autenticacion.storage.daos.ldap
 
 import java.util
-import javax.naming.directory.{ Attributes, SearchControls, SearchResult }
-import javax.naming.ldap.{ InitialLdapContext, LdapContext }
-import javax.naming.{ Context, NamingEnumeration }
+import javax.naming.directory.{Attributes, SearchControls, SearchResult}
+import javax.naming.ldap.{InitialLdapContext, LdapContext}
+import javax.naming.{Context, NamingEnumeration}
 
 import co.com.alianza.commons.enumerations.TiposCliente
 import co.com.alianza.persistence.dto.UsuarioLdapDTO
+import co.com.alianza.util.ConfigApp
+import com.typesafe.config.Config
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 case class AlianzaLdapDAO() extends AlianzaLdapDAOs {
 
@@ -20,12 +22,14 @@ case class AlianzaLdapDAO() extends AlianzaLdapDAOs {
    *         Throws javax.naming.NamingException if authentication failed
    */
   def getLdapContext(username: String, password: String, tipoUsuario: Int)(implicit executionContext: ExecutionContext): Future[LdapContext] = Future {
+    implicit val conf: Config = ConfigApp.conf
     println(tipoUsuario)
-    println(TiposCliente.comercialFiduciaria.id)
-    println(TiposCliente.comercialValores.id)
     val organization: String = if (tipoUsuario == TiposCliente.comercialFiduciaria.id) "fiduciaria" else "valores"
-    val host: String = "ldap://172.20.0.50:389" //TODO: ConfigReader.readString(s"ldap.$organization.host")
-    val domain: String = "alianza0" //TODO: ConfigReader.readString(s"ldap.$organization.domain")
+    println(organization)
+    val host: String =  conf.getString(s"ldap.$organization.host")
+    println(host)
+    val domain: String = conf.getString(s"ldap.$organization.domain")
+    println(domain)
     // CONNECTION EN  ENVIRONMENT
     val environment = new util.Hashtable[String, String]()
     environment.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
@@ -47,8 +51,8 @@ case class AlianzaLdapDAO() extends AlianzaLdapDAOs {
     // SEARCH FILTER
     val filter: String = s"(&(&(objectClass=person)(objectCategory=user))(sAMAccountName=$user))"
     // QUERY
-    //TODO: val searchContext = if (userType == TiposCliente.comercialFiduciaria.id) "DC=Alianza,DC=com,DC=co" else "DC=alianzavaloresint,DC=com"
-    val searchContext = "DC=Alianza,DC=com,DC=co"
+    val searchContext = if (userType == TiposCliente.comercialFiduciaria.id) "DC=Alianza,DC=com,DC=co" else "DC=alianzavaloresint,DC=com"
+    println(searchContext)
     val search: NamingEnumeration[SearchResult] = ctx.search(searchContext, filter, getSearchControls)
     // USER INSTANCE
     val userInstance: Option[UsuarioLdapDTO] = search.hasMore match {
