1aa32a72f8e516509b41c6d9c71013c794603fd1
alexandra <alexandraavendano>
2016-08-05 10:54:09 -0500

    Estructura de datos finalizada de UsuarioAdminComercial y UsuarioComercial

--- persistence/src/main/scala/co/com/alianza/persistence/entities/UsuarioAdminComercial.scala
+++ persistence/src/main/scala/co/com/alianza/persistence/entities/UsuarioAdminComercial.scala
@@ -6,26 +6,23 @@ import CustomDriver.simple._
 /**
  * Created by s4n on 2016
  */
-case class UsuarioComercialAdmin(id: Int, correo: String, fechaActualizacion: Timestamp, usuario: String, identificacion: String, tipoIdentificacion: Int,
-  estado: Int, contrasena: Option[String], token: Option[String], numeroIngresosErroneos: Int, ipUltimoIngreso: Option[String],
-  fechaUltimoIngreso: Option[Timestamp])
+case class UsuarioComercialAdmin(id: Int, correo: String, usuario: String, identificacion: String, tipoIdentificacion: Int, estado: Int,
+  contrasena: Option[String], token: Option[String], ipUltimoIngreso: Option[String], fechaUltimoIngreso: Option[Timestamp], fechaActualizacion: Timestamp)
 
 class UsuarioComercialAdminTable(tag: Tag) extends Table[UsuarioComercialAdmin](tag, "USUARIO_COMERCIAL_ADMIN") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc) // This is the primary key column
   def correo = column[String]("CORREO")
-  def fechaActualizacion = column[Timestamp]("FECHA_ACTUALIZACION")
   def usuario = column[String]("USUARIO")
   def identificacion = column[String]("IDENTIFICACION")
   def tipoIdentificacion = column[Int]("TIPO_IDENTIFICACION")
   def estado = column[Int]("ESTADO")
   def contrasena = column[Option[String]]("CONTRASENA")
   def token = column[Option[String]]("TOKEN")
-  def numeroIngresosErroneos = column[Int]("NUMERO_INGRESOS_ERRONEOS")
   def ipUltimoIngreso = column[Option[String]]("IP_ULTIMO_INGRESO")
   def fechaUltimoIngreso = column[Option[Timestamp]]("FECHA_ULTIMO_INGRESO")
-  def tokenActivacion = column[Option[Timestamp]]("TOKEN_ACTIVACION")
+  def fechaActualizacion = column[Timestamp]("FECHA_ACTUALIZACION")
 
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = (id, correo, fechaActualizacion, usuario, identificacion, tipoIdentificacion, estado, contrasena, token,
-    numeroIngresosErroneos, ipUltimoIngreso, fechaUltimoIngreso) <> (UsuarioComercialAdmin.tupled, UsuarioComercialAdmin.unapply)
+  def * = (id, correo, usuario, identificacion, tipoIdentificacion, estado, contrasena, token, ipUltimoIngreso, fechaUltimoIngreso, fechaActualizacion) <> (
+    UsuarioComercialAdmin.tupled, UsuarioComercialAdmin.unapply)
 }
--- persistence/src/main/scala/co/com/alianza/persistence/entities/UsuarioComercial.scala
+++ persistence/src/main/scala/co/com/alianza/persistence/entities/UsuarioComercial.scala
@@ -6,22 +6,19 @@ import CustomDriver.simple._
 /**
  * Created by s4n on 2016
  */
-case class UsuarioComercial(id: Int, correo: String, fechaActualizacion: Timestamp, identificacion: String, tipoIdentificacion: Int, estado: Int,
-  contrasena: String, token: Option[String], numeroIngresosErroneos: Int, ipUltimoIngreso: Option[String], fechaUltimoIngreso: Option[Timestamp])
+case class UsuarioComercial(id: Int, correo: String, identificacion: String, tipoIdentificacion: Int, estado: Int, token: Option[String],
+  ipUltimoIngreso: Option[String], fechaUltimoIngreso: Option[Timestamp])
 
 class UsuarioComercialTable(tag: Tag) extends Table[UsuarioComercial](tag, "USUARIO_COMERCIAL") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def correo = column[String]("CORREO")
-  def fechaActualizacion = column[Timestamp]("FECHA_ACTUALIZACION")
   def identificacion = column[String]("IDENTIFICACION")
   def tipoIdentificacion = column[Int]("TIPO_IDENTIFICACION")
   def estado = column[Int]("ESTADO")
-  def contrasena = column[String]("CONTRASENA")
   def token = column[Option[String]]("TOKEN")
-  def numeroIngresosErroneos = column[Int]("NUMERO_INGRESOS_ERRONEOS")
   def ipUltimoIngreso = column[Option[String]]("IP_ULTIMO_INGRESO")
   def fechaUltimoIngreso = column[Option[Timestamp]]("FECHA_ULTIMO_INGRESO")
 
-  def * = (id, correo, fechaActualizacion, identificacion, tipoIdentificacion, estado, contrasena, token, numeroIngresosErroneos, ipUltimoIngreso,
-    fechaUltimoIngreso) <> (UsuarioComercial.tupled, UsuarioComercial.unapply)
+  def * = (id, correo, identificacion, tipoIdentificacion, estado, token, ipUltimoIngreso, fechaUltimoIngreso) <> (UsuarioComercial.tupled,
+    UsuarioComercial.unapply)
 }
