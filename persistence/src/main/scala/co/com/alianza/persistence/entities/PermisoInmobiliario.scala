ffad49407ab7ee7ff881a67915c4cc95d6075a40
alexandra <alexandraavendano>
2016-09-22 14:14:55 -0500

    Renombrados las columans de la tabla PermisosAgenteInmob

--- persistence/src/main/scala/co/com/alianza/persistence/entities/PermisoInmobiliario.scala
+++ persistence/src/main/scala/co/com/alianza/persistence/entities/PermisoInmobiliario.scala
@@ -10,8 +10,8 @@ case class PermisoAgenteInmobiliario(idAgente: Int, proyecto: Int, fideicomiso: 
 class PermisoInmobiliarioTable(tag: Tag) extends Table[PermisoAgenteInmobiliario](tag, "PERMISO_INMOBILIARIO") {
 
   def idAgente = column[Int]("ID_AGENTE_INMOBILIARIO")
-  def proyecto = column[Int]("NUMERO_PROYECTO")
-  def fideicomiso = column[Int]("NUMERO_FIDEICOMISO")
+  def proyecto = column[Int]("ID_PROYECTO")
+  def fideicomiso = column[Int]("ID_FIDEICOMISO")
   def tipoPermiso = column[Int]("ID_PERMISO")
 
   def pk = primaryKey("PERMISO_AGENTE_PK", (idAgente, proyecto, fideicomiso, tipoPermiso))
