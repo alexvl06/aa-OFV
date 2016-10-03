52f57a9b09a84e3ec51bad3e43f65be5a2637c9a
alexandra <alexandraavendano>
2016-10-03 11:02:05 -0500

    Mock de identificación para agente inmobiliario

--- services/src/main/scala/portal/transaccional/autenticacion/service/drivers/autenticacion/AutenticacionEmpresaDriverRepository.scala
+++ services/src/main/scala/portal/transaccional/autenticacion/service/drivers/autenticacion/AutenticacionEmpresaDriverRepository.scala
@@ -180,7 +180,8 @@ case class AutenticacionEmpresaDriverRepository(
       caducidad <- usuarioAdminRepo.validarCaducidadContrasena(TiposCliente.clienteAdministrador, usuario, reglaDias.valor.toInt)
       actualizar <- usuarioAdminRepo.actualizarInfoUsuario(usuario, ip)
       inactividad <- configuracionRepo.getConfiguracion(TiposConfiguracion.EXPIRACION_SESION.llave)
-      token <- generarToken(usuario, ip, inactividad.valor, cliente.wcli_constructor)
+      //token <- generarToken(usuario, ip, inactividad.valor, cliente.wcli_constructor)
+      token <- generarToken(usuario, ip, inactividad.valor, "S")
       sesion <- sesionRepo.crearSesion(token, inactividad.valor.toInt, Option(EmpresaDTO.entityToDto(empresa)))
       asociarToken <- usuarioAdminRepo.actualizarToken(usuario.id, AesUtil.encriptarToken(token))
       respuestas <- respuestasRepo.getRespuestasById(usuario.id)
@@ -226,7 +227,7 @@ case class AutenticacionEmpresaDriverRepository(
   }
 
   private def generarToken(usuario: UsuarioEmpresarialAdmin, ip: String, inactividad: String, esConstructor: String) = {
-    if (esConstructor == "S") {
+    if (esConstructor == "S" && usuario.identificacion == "890911431") {
       generarTokenAdmin(usuario, ip, inactividad, TiposCliente.clienteAdminInmobiliario)
     } else {
       generarTokenAdmin(usuario, ip, inactividad, TiposCliente.clienteAdministrador)
