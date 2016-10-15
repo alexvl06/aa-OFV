bf90927f3b3a8571dd6345506436e5c30ef2d8b0
Rodrigo cifuentes <rodrigocifuentes@seven4n.com>
2016-10-14 19:58:35 -0500

    println estado del pin

--- services/src/main/scala/portal/transaccional/autenticacion/service/web/agenteInmobiliario/AgenteImobiliarioPinService.scala
+++ services/src/main/scala/portal/transaccional/autenticacion/service/web/agenteInmobiliario/AgenteImobiliarioPinService.scala
@@ -49,7 +49,9 @@ case class AgenteImobiliarioPinService(
       onComplete(validacionF) {
         case Success(validacion) => validacion match {
           case Right(_) => complete(StatusCodes.OK)
-          case Left(estadoPin) => complete(StatusCodes.Conflict)
+          case Left(estadoPin) =>
+            println(s"ESTADO DEL PIN!!!!!!!!!!!! $estadoPin")
+            complete(StatusCodes.Conflict)
         }
         case Failure(exception) =>
           exception.printStackTrace()
