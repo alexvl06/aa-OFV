01315033056d364c3030c58733851e95959a7b7e
alexandra <alexandraavendano>
2016-11-28 17:13:33 -0500

    Arreglo mejorado para mostrar permisos asignables.

--- services/src/main/scala/portal/transaccional/autenticacion/service/drivers/permisoAgenteInmobiliario/PermisoAgenteInmobiliarioDriverRepository.scala
+++ services/src/main/scala/portal/transaccional/autenticacion/service/drivers/permisoAgenteInmobiliario/PermisoAgenteInmobiliarioDriverRepository.scala
@@ -47,7 +47,7 @@ case class PermisoAgenteInmobiliarioDriverRepository(alianzaDao: AlianzaDAO, usu
   def getRecurso(idUser: Int, tiposCliente: TiposCliente, isMatriz: Boolean): Future[Seq[RecursoGraficoInmobiliario]] = {
     tiposCliente match {
       case TiposCliente.agenteInmobiliario => alianzaDao.getAgentResourcesById(idUser)
-      case TiposCliente.agenteInmobiliarioInterno =>
+      case x if tiposCliente == TiposCliente.agenteInmobiliarioInterno && !isMatriz =>
         alianzaDao.getAdminResourcesVisible(PerfilInmobiliarioEnum.agenteInterno.toString)
       case x if isMatriz => alianzaDao.getAdminResourcesVisible(PerfilInmobiliarioEnum.agente.toString).map(_.filter(_.administrable))
       case _ => alianzaDao.getAdminResourcesVisible(PerfilInmobiliarioEnum.admin.toString)
